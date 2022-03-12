745b1649082b72b357411302521b24bd8557657a
Conno732 <connorscottwalters@gmail.com>
2022-03-12 14:40:31 -0600

    clean up

--- dist/content.js
+++ dist/content.js
@@ -6,41 +6,38 @@ canvas.setAttribute("height", 350);
 video.setAttribute("width", 350);
 video.setAttribute("height", 350);
 if (document.getElementById("password") != null) {
-  document
-    .getElementById("password")
-    .addEventListener("click", () => {
-      // window.open(
-      //   "https://conno732.github.io/Alert-Page-For-ShoulderGuard/",
-      //   "_blank"
-      // );
-      var context = canvas.getContext("2d");
-      canvas.width = 350;
-      canvas.height = 350;
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then(function (stream) {
-          video.srcObject = stream;
-          video
-            .play()
-            .then(() => {
-              context.drawImage(video, 0, 0, 350, 350);
-              var data = canvas.toDataURL("image/png");
-              console.log(data);
+  document.getElementById("password").addEventListener("click", () => {
+    // window.open(
+    //   "https://conno732.github.io/Alert-Page-For-ShoulderGuard/",
+    //   "_blank"
+    // );
+    var context = canvas.getContext("2d");
+    canvas.width = 350;
+    canvas.height = 350;
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video
+          .play()
+          .then(() => {
+            context.drawImage(video, 0, 0, 350, 350);
+            var data = canvas.toDataURL("image/png");
+            console.log(data);
 
-              var xhr = new XMLHttpRequest();
-              xhr.open("POST", "heroku");
-              xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4) {
-                  console.log(xhr.status);
-                  console.log(xhr.responseText);
-                }
-              };
-              xhr.send(data);
-            })
-            .then(() => {
-              video.srcObject.getTracks()[0].stop();
-            });
-        });
-    })
-    .then(() => {});
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://conno732.github.io/ShoulderGuard/");
+            xhr.onreadystatechange = function () {
+              if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+              }
+            };
+            xhr.send(data);
+          })
+          .then(() => {
+            video.srcObject.getTracks()[0].stop();
+          });
+      });
+  });
 }
