1fd84f94d4fc18438240d736046375c91a3511f9
Conno732 <connorscottwalters@gmail.com>
2022-03-12 17:14:35 -0600

    ads

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from typing_extensions import Required
 from urllib import request
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource#, reqparse
 from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-taskArgs = reqparse.RequestParser()
-taskArgs.add_arguement("data", type=str, requiered = True)
+#taskArgs = reqparse.RequestParser()
+#taskArgs.add_arguement("data", type=str, requiered = True)
 
 class HandleRequests(Resource):
     def get(self):
         return {"data": "sda"}
     def post(self):
-        reqData = taskArgs.parse_args()
-        return reqData
+       # reqData = taskArgs.parse_args()
+        return {reqData: "hi"}
 
 api.add_resource(HandleRequests, "/")
 
