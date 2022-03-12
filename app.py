8d55342e2165e723aa44de62f2326119d3755c41
Conno732 <connorscottwalters@gmail.com>
2022-03-12 17:19:20 -0600

    adsd

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+parser = reqparse.RequestParser()
+parser.add_argument('data')
 class HandleRequests(Resource):
     def get(self):
         return {"data": "sda"}
