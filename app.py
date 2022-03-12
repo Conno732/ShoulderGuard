5b5120039870c7c719fafd8fbac4da7a80caa91b
Conno732 <connorscottwalters@gmail.com>
2022-03-12 16:14:32 -0600

    asdas

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "<p>Hello World</p>"
+class HandleRequests(Resource):
+    def get(self):
+        return {"Hello World"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
